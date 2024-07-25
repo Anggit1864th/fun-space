2ab9e925b429e774bf98babd11c72a2ffdf4d7c0
anggit1864th <dokjakim519@gmail.com>
2024-07-25 13:51:50 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,6 @@
+def luas_segitiga():
+    a = int (input("masukkan alas segitiga :"))
+    t = int (input("masukkan alas segitiga :"))
+    luas = a * t / 2
+    print("Luas segitiga :", luas)
+    luas_segitiga()
